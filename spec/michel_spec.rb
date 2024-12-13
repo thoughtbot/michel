dfe471f43cac8122b106ddcbb0f4f796a3387c69
Aji Slater <aji.slater@gmail.com>
2024-12-13 12:47:39 -0600

    initial commit

---
+++ spec/michel_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Michel do
+  it "has a version number" do
+    expect(Michel::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
