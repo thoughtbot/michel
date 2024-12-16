2a646079f552cae11369539c6438eb735041d477
Sally Hall <sally@thoughtbot.com>
2024-12-16 11:07:01 -0500

    fix require statement

--- lib/michel.rb
+++ lib/michel.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "michel/version"
-require "michel/generators"
+require "michel/generators/michel_generator"
 
 module Michel
   class Error < StandardError; end
