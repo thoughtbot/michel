8669605cff1efad3af3aa146f72cd0c8456f1eb7
Sally Hall <sally@thoughtbot.com>
2025-08-22 15:05:01 -0400

    Extract template and config variables

--- lib/michel.rb
+++ lib/michel.rb
@@ -7,10 +7,58 @@ module Michel
   class Error < StandardError; end
 
   mattr_accessor :resource_class_name
+  mattr_accessor :booking_class_name
+  mattr_accessor :availability_class_name
 
-  @@resource_class_name = "Resource"
+  @@resource_class_name = "Banana"
+  @@booking_class_name = "Wheel"
+  @@availability_class_name = "Squirrel"
 
   def self.setup
     yield self
+  end
+
+  def self.resource_class_symbol
+    @@resource_class_name.downcase.to_sym
+  end
+
+  def self.resource_class_foreign_id
+    @@resource_class_name.foreign_key
+  end
+
+  def self.resource_class_underscore
+    @@resource_class_name.underscore
+  end
+
+  def self.booking_class_symbol
+    @@booking_class_name.downcase.to_sym
+  end
+
+  def self.booking_class_table_name
+    @@booking_class_name.titleize.pluralize
+  end
+
+  def self.booking_class_foreign_id
+    @@booking_class_name.foreign_key
+  end
+
+  def self.booking_class_underscore
+    @@booking_class_name.underscore
+  end
+
+  def self.availability_class_symbol
+    @@availability_class_name.downcase.to_sym
+  end
+
+  def self.availability_class_table_name
+    @@availability_class_name.titleize.pluralize
+  end
+
+  def self.availability_class_foreign_id
+    @@availability_class_name.foreign_key
+  end
+
+  def self.availability_class_underscore
+    @@availability_class_name.underscore
   end
 end
